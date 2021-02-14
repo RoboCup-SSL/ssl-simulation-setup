8c2bfd8c1e3871af1a2dd07bacea734a9945ac1e
Nicolai Ommer <nicolai.ommer@gmail.com>
2021-02-14 11:00:29 +0100

    Enhance vnc image

--- src/ubuntu-vnc/startup.sh
+++ src/ubuntu-vnc/startup.sh
@@ -3,13 +3,6 @@
 # Fail on errors
 set -eou pipefail
 
-# correct forwarding of shutdown signal
-cleanup () {
-    kill -s SIGTERM $!
-    exit 0
-}
-trap cleanup SIGINT SIGTERM
-
 echo "Cleanup old VNC server locks"
 vncserver -kill "$DISPLAY" &>> .vnc_startup.log \
     || rm -rfv /tmp/.X*-lock /tmp/.X11-unix &>> .vnc_startup.log \
@@ -24,7 +17,4 @@ echo "$VNC_PW" | vncpasswd -f >> "$PASSWD_PATH"
 chmod 600 "$PASSWD_PATH"
 
 echo "Starting VNC server"
-vncserver "$DISPLAY" -depth "$VNC_COL_DEPTH" -geometry "$VNC_RESOLUTION" -fg -localhost no
-
-#echo "Starting window manager"
-#/bin/bash ./wm_startup.sh &>> .wm_startup.log
+exec vncserver "$DISPLAY" -depth "$VNC_COL_DEPTH" -geometry "$VNC_RESOLUTION" -fg -localhost no

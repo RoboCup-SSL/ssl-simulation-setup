680327e4b713ee33a8d24f31d0369d59b1754efc
Nicolai Ommer <nicolai.ommer@gmail.com>
2021-02-20 18:47:58 +0100

    Add terraform config

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default     = "10.1.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default     = "10.1.0.0/24"
+}
+variable "region" {
+  description = "The region Terraform deploys your instance"
+}

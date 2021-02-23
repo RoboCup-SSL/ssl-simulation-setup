9bbb6be6454609b93628d31e328ff73bfaddb028
Nicolai Ommer <nicolai.ommer@gmail.com>
2021-02-23 15:56:11 +0100

    Add unzip to ubuntu base image

--- src/ubuntu-vnc/setup.sh
+++ src/ubuntu-vnc/setup.sh
@@ -8,7 +8,7 @@ apt-get install -y \
   supervisor icewm xterm xfonts-base xauth xinit \
   tigervnc-standalone-server \
   openssh-server \
-  vim wget git
+  vim wget git unzip
 
 apt-get purge -y pm-utils xscreensaver*
 apt-get autoremove -y

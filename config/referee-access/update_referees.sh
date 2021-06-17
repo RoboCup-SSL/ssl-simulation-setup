ca6b54104a5a42dd0b8a4bb3df0e57ad192f8ec2
Nicolai Ommer <nicolai.ommer@gmail.com>
2021-06-17 12:38:54 +0200

    Fix cronjob scripts

--- config/referee-access/update_referees.sh
+++ config/referee-access/update_referees.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # Lookup the current referee assignment
-python "${SCRIPT_DIR}/update_referee_caddy_passwords.py" 
+"${SCRIPT_DIR}"/update_referee_caddy_passwords.py 
 
 # Update the caddy config file with the new list of passwords
 "${SCRIPT_DIR}"/../caddy/generate_caddyfile.py

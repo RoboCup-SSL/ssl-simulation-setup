474cae3b5ef22fcf04556b86f24aa0c69ec41e40
Nicolai Ommer <nicolai.ommer@gmail.com>
2024-02-02 21:21:33 +0100

    Fix local CA installation for caddy

--- caddy/install_local_CA.sh
+++ caddy/install_local_CA.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # Get the generated local CA from caddy from inside the running container
-docker cp ssl-simulation-setup_caddy_1:/data/caddy/pki/authorities/local/root.crt "${SCRIPT_DIR}/local-ca.crt"
+docker compose cp caddy:/data/caddy/pki/authorities/local/root.crt "${SCRIPT_DIR}/local-ca.crt"
 
 # The CA can now be installed in your system or in your web browser directly
 

da0244c2a54eb9a355f0a573fdba58167b475980
Nicolai Ommer <nicolai.ommer@gmail.com>
2024-02-02 21:11:28 +0100

    Fix caddy CLI syntax

--- config/caddy/update_caddy_config.sh
+++ config/caddy/update_caddy_config.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 export COMPOSE_INTERACTIVE_NO_CLI=1
 
 # Reload caddyfile inside running docker container
-docker compose -f "${SCRIPT_DIR}/../../docker-compose.yaml" exec -T caddy caddy reload -config /etc/caddy/Caddyfile
+docker compose -f "${SCRIPT_DIR}/../../docker-compose.yaml" exec -T caddy caddy reload --config /etc/caddy/Caddyfile

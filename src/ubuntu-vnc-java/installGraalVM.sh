f23625eaf4b05dd9e8cad59d7668b8a4b965febe
Nicolai Ommer <nicolai.ommer@gmail.com>
2024-02-02 19:26:31 +0100

    Update ubuntu-vnc base images

--- src/ubuntu-vnc-java/installGraalVM.sh
+++ src/ubuntu-vnc-java/installGraalVM.sh
@@ -4,8 +4,8 @@ set -e
 
 mkdir -p /usr/lib/jvm/
 cd /usr/lib/jvm/ || exit 1
-wget https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.0.0.2/graalvm-ce-java17-linux-amd64-22.0.0.2.tar.gz -q -O - | tar xz
-update-alternatives --install /usr/bin/java java /usr/lib/jvm/graalvm-ce-java17-22.0.0.2/bin/java 100
-update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/graalvm-ce-java17-22.0.0.2/bin/javac 100
-update-alternatives --set java /usr/lib/jvm/graalvm-ce-java17-22.0.0.2/bin/java
-update-alternatives --set javac /usr/lib/jvm/graalvm-ce-java17-22.0.0.2/bin/javac
+wget https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-21.0.2/graalvm-community-jdk-21.0.2_linux-x64_bin.tar.gz -q -O - | tar xz
+update-alternatives --install /usr/bin/java java /usr/lib/jvm/graalvm-community-openjdk-21.0.2+13.1/bin/java 100
+update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/graalvm-community-openjdk-21.0.2+13.1/bin/javac 100
+update-alternatives --set java /usr/lib/jvm/graalvm-community-openjdk-21.0.2+13.1/bin/java
+update-alternatives --set javac /usr/lib/jvm/graalvm-community-openjdk-21.0.2+13.1/bin/javac
